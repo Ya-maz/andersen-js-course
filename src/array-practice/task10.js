43258ab4b0d541592fc8504c04be07ecf2ee2ae3
Almaz Yakhin <vochko50@gmail.com>
2021-08-26 21:50:21 +0300

    [es-features-done] array-practice-done

--- src/array-practice/task10.js
+++ src/array-practice/task10.js
@@ -10,11 +10,17 @@
 
 // Раскомментировать нижнее, при начале реализации
 
-// const meetups = [
-//   { name: 'JavaScript', isActive: true, members: 100 },
-//   { name: 'Angular', isActive: true, members: 900 },
-//   { name: 'Node', isActive: false, members: 600 },
-//   { name: 'React', isActive: true, members: 500 },
-// ];
+const meetups = [
+  { name: 'JavaScript', isActive: true, members: 100 },
+  { name: 'Angular', isActive: true, members: 900 },
+  { name: 'Node', isActive: false, members: 600 },
+  { name: 'React', isActive: true, members: 500 },
+];
 
-// function membersOnActiveMeetups(meetups) {}
+export default function membersOnActiveMeetups(meetups) {
+  const active = meetups.filter(item => item.isActive === true);
+  return active.reduce((total, current) => total + current.members, 0);
+}
+console.group('task10 info:');
+console.log(membersOnActiveMeetups(meetups));
+console.groupEnd();
